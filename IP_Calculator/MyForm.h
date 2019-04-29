23e498f6f61e2f8c2703ba246b2d0252ea795e86
avdeev kirill <kirill.avdeev2000@yandex.ru>
2019-04-29 10:07:13 +0700

    Changed name and size of textboxes and button

--- IP_Calculator/MyForm.h
+++ IP_Calculator/MyForm.h
@@ -66,48 +66,48 @@ namespace IPCalculator {
 			this->label1->AutoSize = true;
 			this->label1->Location = System::Drawing::Point(39, 9);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(46, 17);
+			this->label1->Size = System::Drawing::Size(64, 17);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"label1";
+			this->label1->Text = L"IP-адрес";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(187, 9);
+			this->label2->Location = System::Drawing::Point(177, 9);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(46, 17);
+			this->label2->Size = System::Drawing::Size(107, 17);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"label2";
+			this->label2->Text = L"Маска подсети";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(12, 29);
+			this->textBox1->Location = System::Drawing::Point(12, 27);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->Size = System::Drawing::Size(118, 22);
 			this->textBox1->TabIndex = 2;
 			// 
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(150, 27);
+			this->comboBox1->Location = System::Drawing::Point(158, 27);
 			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(121, 24);
+			this->comboBox1->Size = System::Drawing::Size(144, 24);
 			this->comboBox1->TabIndex = 3;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(94, 89);
+			this->button1->Location = System::Drawing::Point(12, 71);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(290, 28);
 			this->button1->TabIndex = 4;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Рассчитать данные сети";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(283, 506);
+			this->ClientSize = System::Drawing::Size(314, 506);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->comboBox1);
 			this->Controls->Add(this->textBox1);
