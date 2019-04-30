4610fa9c5dc7b940a243a331d195e6f0b848e421
dmitrymack <47584517+dmitrymack@users.noreply.github.com>
2019-04-30 16:52:43 +0700

    Added calculation of broadcast

--- IP_Calculator/calculations.h
+++ IP_Calculator/calculations.h
@@ -6,3 +6,4 @@ std::string network_adress(std::string ip, std::string mask);
 std::string wildcard(std::string mask);
 std::string first_host(std::string netw_adr, std::string mask);
 std::string last_host(std::string wildcard, std::string netw_adr);
+std::string broadcast(std::string netw_adr, std::string wildcard);
