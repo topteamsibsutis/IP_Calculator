adfa030dcaea5c45abc033e945078665193b49de
dmitrymack <47584517+dmitrymack@users.noreply.github.com>
2019-05-10 12:22:14 +0700

    Added validation check

--- IP_Calculator/calculations.cpp
+++ IP_Calculator/calculations.cpp
@@ -2,14 +2,42 @@
 #include <stdlib.h>
 #include <string>
 
-int transform(std::string object, int* object_int)
+
+int check(std::string object)
 {
-	int i = 0, j = 0, a = 0;
+	int object_int[4];
+	int i = 0, j = 0, a = 0, quan = 0;
 	char part[4][4];
 	while (object[i] != '\0') {
 		if ((object[i] < 48 || object[i] > 57) && object[i] != '.')
 			return 1;
 
+		if (object[i] == '.') {
+			i++;
+			a = 0;
+			j++;
+			quan++;
+			if (quan > 3) return 3;
+		}
+
+		part[j][a] = object[i];
+		a++;
+		i++;
+	}
+	if (quan < 3) return 3;
+	for (i = 0; i < 4; i++) {
+		object_int[i] = atoi(part[i]);
+		if (object_int[i] < 0 || object_int[i] > 255)
+			return 2;
+	}
+	return 0;
+}
+void transform(std::string object, int* object_int)
+{
+	int i = 0, j = 0, a = 0;
+	char part[4][4];
+	while (object[i] != '\0') {
+		
 		if (object[i] == '.') {
 			i++;
 			a = 0;
@@ -23,10 +51,7 @@ int transform(std::string object, int* object_int)
 
 	for (i = 0; i < 4; i++) {
 		object_int[i] = atoi(part[i]);
-		if (object_int[i] < 0 || object_int[i] > 255)
-			return 1;
 	}
-	return 0;
 }
 
 std::string network_adress(std::string ip, std::string mask)
