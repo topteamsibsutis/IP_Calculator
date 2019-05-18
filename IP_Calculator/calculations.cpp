e3f8ae4ef34800b1d5dde176dcd9c7603d6026ee
dmitrymack <47584517+dmitrymack@users.noreply.github.com>
2019-05-18 12:36:31 +0700

    Added the validation check on branch develop_logic

--- IP_Calculator/calculations.cpp
+++ IP_Calculator/calculations.cpp
@@ -4,14 +4,42 @@
 
 using namespace std;
 
-int transform(string object, int* object_int)
+int check(std::string object)
 {
-	int i = 0, j = 0, a = 0;
+	int object_int[4];
+	int i = 0, j = 0, a = 0, quan = 0;
 	char part[4][4];
 	while (object[i] != '\0') {
 		if ((object[i] < 48 || object[i] > 57) && object[i] != '.')
 			return 1;
 
+		if (object[i] == '.') {
+			i++;
+			a = 0;
+			j++;
+			quan++;
+			if (quan > 3) return 3;
+		}
+
+		part[j][a] = object[i];
+		a++;
+		i++;
+	}
+	if (quan < 3) return 3;
+	for (i = 0; i < 4; i++) {
+		object_int[i] = atoi(part[i]);
+		if (object_int[i] < 0 || object_int[i] > 255)
+			return 2;
+	}
+	return 0;
+}
+
+void transform(string object, int* object_int)
+{
+	int i = 0, j = 0, a = 0;
+	char part[4][4];
+	while (object[i] != '\0') {
+			
 		if (object[i] == '.') {
 			i++;
 			a = 0;
@@ -25,10 +53,8 @@ int transform(string object, int* object_int)
 
 	for (i = 0; i < 4; i++) {
 		object_int[i] = atoi(part[i]);
-		if (object_int[i] < 0 || object_int[i] > 255)
-			return 1;
+		
 	}
-	return 0;
 }
 
 string network_adress(string ip, string mask)
--- IP_Calculator/calculations.h
+++ IP_Calculator/calculations.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 
+int check(std::string object);
 std::string network_adress(std::string ip, std::string mask);
 std::string wildcard(std::string mask);
 std::string first_host(std::string netw_adr, std::string mask);
