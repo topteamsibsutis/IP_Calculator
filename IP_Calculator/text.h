ff02ce6a573a5c260cb380f4aad23acc7a634198
Кирилл Авдеев <kirill.avdeev2000@yandex.ru>
2019-05-19 15:04:44 +0700

    Now program created text file Data with all calculated informations.

---
+++ IP_Calculator/text.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+void create_file(std::string ip,std::string netmask,std::string wildcard, std::string broadcast, std::string address, std::string first_host, std::string last_host, std::string quan_ip_all, std::string quan_ip_aviable);
