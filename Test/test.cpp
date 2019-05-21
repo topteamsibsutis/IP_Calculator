32bf59db48da34477cac744977d959f4d04d34de
avdeev kirill <kirill.avdeev2000@yandex.ru>
2019-05-21 11:31:24 +0700

    Creating test for check_ip_input

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,8 +1,23 @@
 #include "pch.h"
 
+int error = 0;
 
+void check_ip_input(std::string ip_input_string)
+{
+	int k = 0;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	while (ip_input_string[k] != '\0')
+	{
+		k++;
+	}
+
+	if (ip_input_string[k - 1] == '.')
+	{
+		error = 1;
+	}
+}
+
+TEST(Check_ip_input, Check_ip_input) {
+	check_ip_input("192.168.0.");
+	EXPECT_EQ(1, error);
 }
